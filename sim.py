8b8cd0caf53b0e57f5f810870504a94a2fda2f7c
Madhavan <madhavanraja99@gmail.com>
2020-03-24 18:00:49 +0530

    Added sample cases

--- sim.py
+++ sim.py
@@ -2,46 +2,20 @@ from autonomous_system import autonomous_system
 from optimize import optimize
 from tabulate import tabulate
 
+from sample_a import systems
+
 
 def main():
-    # as1 = autonomous_system(10)
-    # as2 = autonomous_system(20)
-    # as3 = autonomous_system(30)
-    # as4 = autonomous_system(40)
-    # as5 = autonomous_system(50)
-    # as6 = autonomous_system(60)
-
-    # as1.neighbours = {as2: 100, as6: 100}
-    # as2.neighbours = {as1: 100, as3: 100}
-    # as3.neighbours = {as2: 100, as4: 100}
-    # as4.neighbours = {as3: 100, as5: 100}
-    # as5.neighbours = {as4: 100, as6: 100}
-    # as6.neighbours = {as1: 100, as5: 100}
-
-    as1 = autonomous_system(1)
-    as2 = autonomous_system(2)
-    as3 = autonomous_system(3)
-    as4 = autonomous_system(4)
-    as5 = autonomous_system(5)
-    as6 = autonomous_system(6)
-
-    as1.neighbours = {as2: 100, as3: 100}
-    as2.neighbours = {as1: 100, as4: 100}
-    as3.neighbours = {as1: 100, as4: 100}
-    as4.neighbours = {as2: 100, as3: 100}
-    as5.neighbours = {as6: 100}
-    as6.neighbours = {as5: 100}
-
-    systems = [as1, as2, as3, as4, as5, as6]
-
     optimize(systems)
 
+    # Display all the systems
     print('Autonomous Systems:')
     print()
     print(tabulate([[asys, asys.local_preference] for asys in systems], headers=[
           'Autonomous System', 'Local Preference'], tablefmt='pretty'))
     print()
 
+    # Display all the neighbours
     for asys in systems:
         print(f'Neighbours of {asys}:')
         print()
@@ -49,6 +23,7 @@ def main():
               'Autonomous System', 'Weight'], tablefmt='pretty'))
         print()
 
+    # Display all the Routing Tables
     for asys in systems:
         print(f'Routing Table for {asys}:')
         print()
